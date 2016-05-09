f3af9c2b92ed5f943b741f5040f07e884c4195f0
TimAndy <xuchonglei@126.com>
2016-05-09 09:00:42 +0800

    【汉化】SSH key was added to your account=>你的账户已添加 SSH 秘钥

--- app/mailers/emails/profile.rb
+++ app/mailers/emails/profile.rb
@@ -20,7 +20,7 @@ module Emails
 
       @current_user = @user = @key.user
       @target_url = user_url(@user)
-      mail(to: @user.notification_email, subject: subject("SSH key was added to your account"))
+      mail(to: @user.notification_email, subject: subject("你的账户已添加 SSH 秘钥"))
     end
   end
 end
