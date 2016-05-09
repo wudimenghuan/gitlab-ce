ed954a327825430b23acef36802867a2dff5e7dd
TimAndy <xuchonglei@126.com>
2016-05-09 09:00:39 +0800

    【汉化】One project failed its last repository check=>一个项目仓库检查失败

--- app/mailers/repository_check_mailer.rb
+++ app/mailers/repository_check_mailer.rb
@@ -1,7 +1,7 @@
 class RepositoryCheckMailer < BaseMailer
   def notify(failed_count)
     if failed_count == 1
-      @message = "One project failed its last repository check"
+      @message = "一个项目仓库检查失败"
     else
       @message = "#{failed_count} 个项目仓库检查失败"
     end
