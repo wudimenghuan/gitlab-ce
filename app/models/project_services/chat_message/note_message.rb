75c3893a5814ca6f84c4e42de3281f8dfa7244aa
xHang <dreampet@gmail.com>
2017-05-26 09:09:14 +0800

    add new translation

--- app/models/project_services/chat_message/note_message.rb
+++ app/models/project_services/chat_message/note_message.rb
@@ -32,8 +32,8 @@ module ChatMessage
 
     def activity
       {
-        title: "#{user_name} #{link('commented on ' + target, note_url)}",
-        subtitle: "in #{project_link}",
+        title: "#{user_name} #{link('评论了 ' + target, note_url)}",
+        subtitle: "于 #{project_link}",
         text: formatted_title,
         image: user_avatar
       }
@@ -42,7 +42,7 @@ module ChatMessage
     private
 
     def message
-      "#{user_name} #{link('commented on ' + target, note_url)} in #{project_link}: *#{formatted_title}*"
+      "#{user_name} #{link('评论了 ' + target, note_url)} 于 #{project_link}: *#{formatted_title}*"
     end
 
     def format_title(title)
@@ -54,21 +54,21 @@ module ChatMessage
     end
 
     def create_issue_note(issue)
-      ["issue #{Issue.reference_prefix}#{issue[:iid]}", issue[:title]]
+      ["问题 #{Issue.reference_prefix}#{issue[:iid]}", issue[:title]]
     end
 
     def create_commit_note(commit)
       commit_sha = Commit.truncate_sha(commit[:id])
 
-      ["commit #{commit_sha}", commit[:message]]
+      ["提交 #{commit_sha}", commit[:message]]
     end
 
     def create_merge_note(merge_request)
-      ["merge request #{MergeRequest.reference_prefix}#{merge_request[:iid]}", merge_request[:title]]
+      ["合并请求 #{MergeRequest.reference_prefix}#{merge_request[:iid]}", merge_request[:title]]
     end
 
     def create_snippet_note(snippet)
-      ["snippet #{Snippet.reference_prefix}#{snippet[:id]}", snippet[:title]]
+      ["代码片段 #{Snippet.reference_prefix}#{snippet[:id]}", snippet[:title]]
     end
 
     def description_message
