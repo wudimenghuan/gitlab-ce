a3638b4b3f40431b521401be48cb827bdfdf6362
xHang <dreampet@gmail.com>
2017-07-04 10:57:24 +0800

    add missing translation

--- app/assets/javascripts/labels_select.js
+++ app/assets/javascripts/labels_select.js
@@ -160,13 +160,13 @@ import IssuableBulkUpdateActions from './issuable_bulk_update_actions';
                 if (showNo) {
                   extraData.unshift({
                     id: 0,
-                    title: 'No Label'
+                    title: '没有标记'
                   });
                 }
                 if (showAny) {
                   extraData.unshift({
                     isAny: true,
-                    title: 'Any Label'
+                    title: '任何标记'
                   });
                 }
                 if (extraData.length) {
